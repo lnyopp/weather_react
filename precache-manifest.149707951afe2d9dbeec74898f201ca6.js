d5aaf9603694e38023d5475b960714aaef95e616
Rumble Roar <lnyopp15@gmail.com>
2020-04-23 15:50:39 -0400

    Updates

---
+++ precache-manifest.149707951afe2d9dbeec74898f201ca6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b1a177e6ee3ebd86a2c039d7139e55",
+    "url": "/weather_react/index.html"
+  },
+  {
+    "revision": "4bfbb5fb897afed183c9",
+    "url": "/weather_react/static/css/main.0526b8f1.chunk.css"
+  },
+  {
+    "revision": "c420fcc7bb8a3c5db7f8",
+    "url": "/weather_react/static/js/2.728b3e92.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather_react/static/js/2.728b3e92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bfbb5fb897afed183c9",
+    "url": "/weather_react/static/js/main.c9d9f070.chunk.js"
+  },
+  {
+    "revision": "67d0d8745056a42be6d1",
+    "url": "/weather_react/static/js/runtime-main.4e8cd0b5.js"
+  }
+]);
